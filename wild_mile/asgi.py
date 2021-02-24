22bf9abd62da00f21163508c9dbfbb17105a3d2b
jamie120 <jamie120@hotmail.com>
2021-02-24 12:20:10 +0000

    Inital commit

---
+++ wild_mile/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for wild_mile project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wild_mile.settings')
+
+application = get_asgi_application()
