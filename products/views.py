2489bc701ae82c561cecb367f4a385511900e3a7
jamie120 <jamie120@hotmail.com>
2021-02-27 08:44:03 +0000

    Updated nav links to accomodate category filtering

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -8,9 +8,17 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
-        'products': products
+        'products': products,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
