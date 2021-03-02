4ecf7b6087cc3212fc550d3d4ce897b22c81e510
jamie120 <jamie120@hotmail.com>
2021-03-02 15:39:44 +0000

    added checkout app and models

--- wild_mile/settings.py
+++ wild_mile/settings.py
@@ -46,9 +46,7 @@ INSTALLED_APPS = [
     'products',
     'conversions',
     'bag',
-
-    # Other
-    'crispy_forms',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -81,10 +79,6 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
-            'builtins': [
-                'crispy_forms.templatetags.crispy_forms_tags',
-                'crispy_forms.templatetags.crispy_forms_field',
-            ]
         },
     },
 ]
