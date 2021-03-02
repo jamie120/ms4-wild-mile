3d286f89fa5dc87dda84b58781eb13f3905201c6
jamie120 <jamie120@hotmail.com>
2021-03-02 13:43:55 +0000

    Added sorting and filtering to conversions

--- conversions/views.py
+++ conversions/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.db.models.functions import Lower
 from django.db.models import Q
 from .models import CamperConversion, Category
 
@@ -12,7 +13,11 @@ def all_conversions(request):
 
     conversions = CamperConversion.objects.all()
     query = None
+    sort = None
+    direction = None
     categories = None
+    all_categories = Category.objects.all()
+    current_category = None
 
     if request.GET:
         if 'q' in request.GET:
@@ -25,15 +30,36 @@ def all_conversions(request):
             queries = Q(name__icontains=query) | Q(conversion_description__icontains=query)
             conversions = conversions.filter(queries)
 
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                conversions = conversions.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            conversions = conversions.order_by(sortkey)
+
         if 'category' in request.GET:
-                categories = request.GET['category'].split(',')
-                conversions = conversions.filter(category__name__in=categories)
-                categories = Category.objects.filter(name__in=categories)
+            categories = request.GET['category'].split(',')
+            conversions = conversions.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+            current_category = request.GET['category']
+            current_category = current_category.replace("_", " ").capitalize()
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'conversions': conversions,
         'search_term': query,
         'current_categories': categories,
+        'current_category': current_category,
+        'all_categories': all_categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'conversions/conversions.html', context)
