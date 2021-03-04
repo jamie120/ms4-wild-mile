9be338a99f577dbb82668f79c5845adf3748559c
jamie120 <jamie120@hotmail.com>
2021-03-04 14:44:19 +0000

    Added stripe elements to checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -7,13 +7,15 @@ from .forms import OrderForm
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
-        messages.error(request, "Theres nothing in your bad at the moment")
+        messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ILRYRGvGwncWji0qtp9Gql6jbMRfCoqAFqTynRfNehJdPbKube9IIHeur4hkckpoV5kvyvqC1zSqC9mvbyVHH3900ab6IbZLw',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
