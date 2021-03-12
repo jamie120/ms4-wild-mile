ceaed7eeb598b586784b53aa5c25b084f75438e9
jamie120 <jamie120@hotmail.com>
2021-03-12 14:19:27 +0000

    Added logic to only render listing content if the following is true: is_active, is superuser or is listing author

--- profiles/models.py
+++ profiles/models.py
@@ -16,7 +16,7 @@ class UserProfile(models.Model):
     order history,
     saved products/listings
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
