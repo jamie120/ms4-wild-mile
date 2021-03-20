7e0d5b9a116be0e551b7f46155e6d1f47b251b57
jamie120 <jamie120@hotmail.com>
2021-03-20 14:02:51 +0000

    added edit product template, view and url

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<product_id>', views.edit_product, name='edit_product'),
     path('<int:product_id>', views.product_detail, name='product_detail'),
     path('listing_levels/', views.listing_levels, name='listing_levels'),
     path('view_plan/<product_id>', views.view_plan, name='view_plan'),
