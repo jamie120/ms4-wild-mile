e885cf623b51d235a85259a59770a922ff910918
jamie120 <jamie120@hotmail.com>
2021-03-20 08:11:08 +0000

    Add templates for about and faq pages

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('faq/', views.faq, name='faq'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to return the index page """
+    return render(request, 'home/about.html')
+
+
+def faq(request):
+    """ A view to return the index page """
+    return render(request, 'home/faq.html')
