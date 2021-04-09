67f7596b963242a6da68473bcbcdd561f633cce3
jamie120 <jamie120@hotmail.com>
2021-04-09 11:02:42 +0000

    Move secret key out of settings.py, adjust debug to conditional statement

--- wild_mile/settings.py
+++ wild_mile/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sa)^6g1j89ppwzw@9x0e8vtho77ylb-p#@wy7fsv2tr2f-19rf'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['wild-mile-ms4.herokuapp.com', 'localhost']
 
