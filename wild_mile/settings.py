47c5cfc49679b3dec57dc3b1d92d359a191087e8
jamie120 <jamie120@hotmail.com>
2021-04-20 08:25:02 +0000

    Update DEBUG status

--- wild_mile/settings.py
+++ wild_mile/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+
+DEBUG = False
 
 # DEBUG = True
 
