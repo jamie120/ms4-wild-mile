cd74fd48ec7c6d0f891ef0d67319bf3aa033a90c
jamie120 <jamie120@hotmail.com>
2021-05-12 13:33:29 +0000

    Update merchandise image sizes

--- products/models.py
+++ products/models.py
@@ -26,7 +26,6 @@ class Product(models.Model):
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
